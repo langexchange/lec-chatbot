d1eab61490c3eb4abdc65ae6a64eddc3eb687b45
vutl <vu.tran10100305@gmail.com>
2023-05-21 22:47:49 +0700

    chore

--- chatbot/chatworker/main.py
+++ chatbot/chatworker/main.py
@@ -3,7 +3,6 @@ import environ
 import os
 import logging
 import json
-import asyncio
 
 env = environ.Env()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -30,36 +29,14 @@ class ChatBotConsumer:
     def register(self, topicName: str, handlerInstance):
       self.handlers[topicName] = handlerInstance
     
+
     def callHandler(self, message):
       topicName = message.topic
       if topicName not in self.handlers:
         logger.error('There is no handler for this {}'.format(topicName))
       logger.info('Call handler {} for msg {}'.format(self.handlers[topicName], message.value))
       self.handlers[topicName](message.value)
-    
-    # def createTopicsIfNotExists(self, num_partitions=1, replication_factor=1):
-    #   admin_client = AdminClient({'bootstrap.servers': self.conf["bootstrap.servers"]})
-    #   topic_metadata = admin_client.list_topics(timeout = 5)
 
-    #   new_topics = []
-    #   for topic in self.handlers:
-    #     if topic not in topic_metadata.topics:
-    #       new_topic = NewTopic(topic=topic, num_partitions=num_partitions, replication_factor=replication_factor)
-    #       new_topics.append(new_topic)
-
-    #     else:
-    #       logger.debug("Topic {} already exists".format(topic))
-
-    #   if not new_topics:
-    #     return
-
-    #   futures = admin_client.create_topics(new_topics)
-    #   for topic_name in futures:
-    #     try:
-    #       futures[topic_name].result()
-    #       logger.debug("Create topic {} with num_partitions {} and replication_factor {}".format(topic_name, num_partitions, replication_factor))
-    #     except Exception as e:
-    #       logger.warn("Failed to create topic {}: {}".format(topic_name, e))
     
     def value_deserializer(self, value: bytes) -> dict:
       try:
@@ -68,6 +45,7 @@ class ChatBotConsumer:
         logger.exception("Message received from %s fail to be parsed", payload)
         raise('Decoding JSON has failed')
       return payload
+    
     
     async def initChatBotConsumer(self):
       self.consumer = aiokafka.AIOKafkaConsumer("chathelper-userinfo", bootstrap_servers= self.bootstrap_servers, group_id= self.group_id, auto_offset_reset= self.auto_offset_reset, enable_auto_commit= self.enable_auto_commit, value_deserializer = self.value_deserializer)
---
+++ test.py
@@ -0,0 +1,9 @@
+import asyncio
+import psycopg
+
+async def main():
+  async with await psycopg.AsyncConnection.connect
+  
+
+if __name__ == "__main__":
+  asyncio.run(main())
